d4901d83ccbc1853bee883acc93e3d73de2eb539
jo-alves22 <jonasadrianoalves@gmail.com>
2023-08-24 20:55:11 -0300

    initial commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../hero/Hero"
+import Navbar from "../navbar/Navbar"
+
+
+function Home() {
+    return(
+        <>
+        <Navbar />
+        <Hero />
+        </>
+    )
+}
+export default Home
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import jonasLogo from '../../assets/Logo.svg'
+import './Navbar.css'
+
+class Navbar extends Component {
+    state={clicked: false};
+    handleClick = () =>{
+       this.setState({clicked:
+        !this.state.clicked})
+    }
+    render() {
+        return (
+            <nav className="navbar">
+                <div>
+                    <img src={jonasLogo} className="logojonas" alt="Jonas logo" />
+                </div>
+                <div>
+                    <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Projetos</a></li>
+                        <li><a href="#">Experiência</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </div>
+                <div id="mobile" onClick={this.handleClick}>
+                    <i id="bar" className={this.state.clicked ?'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                
+            </nav>
+        )
+    }
+}
+export default Navbar
