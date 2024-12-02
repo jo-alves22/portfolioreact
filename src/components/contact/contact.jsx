ae3ffa9a5362b7ad8c390238e4c4f997ab382987
Jonas Alves <jonasadrianoalves@gmail.com>
2024-12-02 20:44:28 -0300

    [feat]:Added contact component

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,11 @@
+import './contact.css'
+
+export default function Contact () {
+    return(
+        <div>
+            <section>
+                <h1 className='titlecontact'>Contato</h1>
+            </section>
+        </div>
+    )
+}
--- src/components/technologies/technologies.jsx
+++
@@ -1,9 +0,0 @@
-import './technologies.css'
-
-export default function Technologies () {
-    return(
-        <div>
-            <h1>Tecnologias e interesses</h1>
-        </div>
-    )
-}
